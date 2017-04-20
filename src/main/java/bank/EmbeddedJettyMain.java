af261db199e9819bb987bacfa7d7e253cb6ba572
MacPherson <maxim.troitsky@gmail.com>
2017-04-21 01:41:22 +0300

    initial

---
+++ src/main/java/bank/EmbeddedJettyMain.java
@@ -0,0 +1,15 @@
+package bank;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+
+public class EmbeddedJettyMain {
+
+	public static void main(String[] args) throws Exception {
+		Server server = new Server(7070);
+		ServletContextHandler handler = new ServletContextHandler(server, "/dashboard");
+		handler.addServlet(DashboardServlet.class, "/");
+		server.start();
+	}
+
+}
